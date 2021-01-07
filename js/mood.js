0061c4d41f68f0fb4232bf7c9b4b8699aa51efea
pete3249 <katelynrpeterson16@gmail.com>
2021-01-07 17:15:47 -0500

    Change mood.js file location

---
+++ js/mood.js
@@ -0,0 +1,33 @@
+class Mood {
+    constructor(attributes) {
+        let whitelist = ["id", "name"]
+        whitelist.forEach(attr => this[attr] = attributes[attr])
+    }
+
+    static all() {
+        fetch("http://localhost:3000/moods")
+            .then(res => res.json())
+            .then(moodObjects => {
+                this.moods = moodObjects.map(moodAttributes => new Mood(moodAttributes))
+                let moods = this.moods.map(mood => mood.render())
+                return this.moods
+            })
+    }
+
+    render() {
+        this.c = document.querySelector("#moodSelection")
+        let br = document.createElement("br")
+
+        this.checkbox ||= document.createElement("INPUT");
+        this.checkbox.setAttribute("type", "checkbox")
+        this.checkbox.dataset.moodId = this.id
+        this.checkbox.id = this.name
+
+        this.label ||= document.createElement("LABEL");
+        this.label.setAttribute("for",`${this.name}`)
+        this.label.innerHTML = this.name
+        this.label.classList.add(..."text-lg font-light".split(" "))
+
+        return this.c.append(br, this.checkbox, this.label)
+    }
+}
