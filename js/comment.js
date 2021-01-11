772341118a960d3bf4acfcc5d60b5c44105a357c
pete3249 <katelynrpeterson16@gmail.com>
2021-01-11 18:44:57 -0500

    Add comment constructor

--- js/comment.js
+++ js/comment.js
@@ -1,5 +1,7 @@
 class Comment {
-    constructor() {
-        
+    constructor(attributes) {
+        let whitelist= ["id", "first_name", "content"]
+        whitelist.forEach(attr => this[attr] = attributes[attr])
     }
+
 }
