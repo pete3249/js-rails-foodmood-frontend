eb158a54e85b1aa090f0cdd065cc4faab4e0cccb
pete3249 <katelynrpeterson16@gmail.com>
2021-01-12 11:53:18 -0500

    Add render method for errors

--- js/flashmessage.js
+++ js/flashmessage.js
@@ -1,5 +1,23 @@
 class FlashMessage {
-    constructor() {
-        
+    constructor({message, type}) {
+        this.message = message;
+        this.type = type;
+        this.render();
+    }
+
+    container() {
+        return this.c ||= document.querySelector("#flash")
+    }
+
+    render() {
+        this.container().textContent = this.message;
+        this.toggleDisplay();
+        setTimeout(() => this.toggleDisplay(), 5000)
+    }
+
+    toggleDisplay() {
+        this.container().classList.toggle("opacity-0")
+        this.container().classList.toggle(this.type =='error'? 'bg-red-300' : 'bg-blue-300')
     }
 }
+
