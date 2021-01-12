7d743ca74f78e343f7d27cf7e93238680662d76e
pete3249 <katelynrpeterson16@gmail.com>
2021-01-12 01:55:12 -0500

    Add create method to make new comment

--- js/comment.js
+++ js/comment.js
@@ -1,7 +1,28 @@
 class Comment {
     constructor(attributes) {
-        let whitelist= ["id", "first_name", "content"]
+        let whitelist= ["id", "name", "review", "recipe_id", "date"]
         whitelist.forEach(attr => this[attr] = attributes[attr])
+    }
+
+    static create(formData) {
+        return fetch("http://localhost:3000/comments", {
+            method: "POST",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res => {
+            if(res.ok) {
+                return res.json();
+            } else {
+                return res.text().then(error => Promise.reject(error));
+            }
+        })
+        .then(commentObject => {
+            debugger
+        })
     }
 
 }
