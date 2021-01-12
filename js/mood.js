4867368a5c13b4134db44cbde75e5a5cb8808f40
pete3249 <katelynrpeterson16@gmail.com>
2021-01-12 10:40:30 -0500

    Refactor static all() by removing headers, catch

--- js/mood.js
+++ js/mood.js
@@ -9,43 +9,29 @@ class Mood {
     }
 
     static all() {
-        return fetch("http://localhost:3000/moods", {
-            headers: {
-              "Accept": "application/json",
-              "Content-Type": "application/json"
-            }
-        })
-            .then(res => {
-                if(res.ok) {
-                    return res.json();
-                } else {
-                    return res.text().then(error => Promise.reject(error));
-                }
-            })
+        return fetch("http://localhost:3000/moods")
+            .then(res => res.json())
             .then(moodObjects => {
-                this.moods = moodObjects.map(moodAttributes => new Mood(moodAttributes));
+                this.moods = moodObjects.map(moodAttributes => new Mood(moodAttributes))
                 let moods = this.moods.map(mood => mood.render());
                 return this.moods;
-            })
-            .catch(error => {
-                console.log(error);
             })
     }
 
     render() {
-        let br = document.createElement("br");
+        let b = document.createElement("br");
 
-        this.checkbox ||= document.createElement("INPUT");
+        this.checkbox ||= document.createElement("input");
         this.checkbox.setAttribute("type", "checkbox");
         this.checkbox.value = this.id;
         this.checkbox.id = this.name;
         this.checkbox.name = "mood";
 
-        this.label ||= document.createElement("LABEL");
+        this.label ||= document.createElement("label");
         this.label.setAttribute("for",`${this.name}`);
-        this.label.innerHTML = this.name;
+        this.label.textContent = this.name;
         this.label.classList.add(..."text-lg font-light".split(" "));
 
-        return Mood.container().append(br, this.checkbox, this.label);
+        return Mood.container().append(b, this.checkbox, this.label);
     }
 }
